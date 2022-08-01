2940deebdf18f39b850f697759e5720ed11761a5
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-08-01 18:52:03 -0400

    merging queries added i app to avoid error/warning

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,33 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import Clients from "./components/Clients";
 
 function App() {
+  //merging queries to avoid warning in console
+  //Cache data may be lost when replacing the clients field of a Query object
+  const cache = new InMemoryCache({
+    typePolicies:{
+      query:{
+        fields:{
+          clients: {
+            merge(existing, incoming, { mergeObjects }){
+              return mergeObjects(existing, incoming);
+            }
+          },
+          projects: {
+            merge(existing, incoming, { mergeObjects }){
+              return mergeObjects(existing, incoming);
+            }
+          },
+        }
+      }
+    }
+  });
+
   const client = new ApolloClient({
     uri: "http://localhost:4444/graphql",
-    cache: new InMemoryCache(),
+    // cache: new InMemoryCache(),
+    cache,
   });
+
   return (
     <>
       <ApolloProvider client={client}>
